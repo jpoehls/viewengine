d75cbbb583cf1e6781e15d103dfce1e674c211ad
Joshua Poehls <joshua@zduck.com>
2013-12-08 15:00:29 -0600

    Added several more tests. Only the nested master page test is failing.

--- viewengine_test.go
+++ viewengine_test.go
@@ -2,30 +2,186 @@ package viewengine
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 )
 
-func Test_SimpleParseAndExecute(t *testing.T) {
+func showWhitespace(input string) string {
+	return strings.Replace(strings.Replace(input, " ", "·", -1), "\t", "⇥", -1)
+}
 
+// Renders a template to a string.
+func (ve *ViewEngine) render(name string, data interface{}) (string, error) {
+	var out bytes.Buffer
+	err := ve.Execute(&out, name, data)
+	if err != nil {
+		return "", err
+	}
+
+	return out.String(), nil
+}
+
+func Test_SimpleMasterPage(t *testing.T) {
 	ve := New()
 
-	Must(ve.Parse("main_layout", `The subject is: {{template "__subject"}}
-The message is: {{template "__content"}}`))
+	Must(ve.Parse("masterpage", `<html>
+	<body>
+		{{template "__body"}}
+	</body>
+</html>`))
 
-	Must(ve.Parse("menu", `MENU`))
+	Must(ve.Parse("homepage", `{{define "__body"}}<p>Page content here.</p>{{end}}
+{{template "masterpage"}}`))
 
-	Must(ve.Parse("page1", `{{define "__subject"}}Page 1 Subject{{end}}
-{{define "__content"}}Page 1 Content{{end}}
-{{optional_template "menu"}}
-{{template "main_layout"}}`))
-
-	var out bytes.Buffer
-	err := ve.Execute(&out, "page1", nil)
+	out, err := ve.render("homepage", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	t.Log(out.String())
+	if out != `
+<html>
+	<body>
+		<p>Page content here.</p>
+	</body>
+</html>` {
+		t.Logf("\"%s\"", out)
+		t.Fatal("Rendered template is incorrect.")
+	}
+}
+
+func Test_SimpleMasterPageWithIncludes(t *testing.T) {
+	ve := New()
+
+	Must(ve.Parse("masterpage", `<html>
+	<body>
+		{{template "__body"}}
+		{{template "snippet"}}
+	</body>
+</html>`))
+
+	Must(ve.Parse("homepage", `{{define "__body"}}<p>Page content here. {{template "snippet"}}</p>{{end}}
+{{template "masterpage"}}`))
+
+	Must(ve.Parse("snippet", `SNIPPET`))
+
+	out, err := ve.render("homepage", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if out != `
+<html>
+	<body>
+		<p>Page content here. SNIPPET</p>
+		SNIPPET
+	</body>
+</html>` {
+		t.Logf("\"%s\"", out)
+		t.Fatal("Rendered template is incorrect.")
+	}
+}
+
+func Test_NestedMasterPage(t *testing.T) {
+	ve := New()
+
+	Must(ve.Parse("masterpage_top", `<html>
+	<body>
+		{{template "__body"}}
+	</body>
+</html>`))
+
+	Must(ve.Parse("masterpage_inner", `{{define "__body"}}<h1>Common Header</h1>
+	{{template "__body"}}{{end}}
+{{template "masterpage_top"}}`))
+
+	Must(ve.Parse("homepage", `{{define "__body"}}<p>Page content here.</p>{{end}}
+{{template "masterpage_inner"}}`))
+
+	out, err := ve.render("homepage", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if out != `
+<html>
+	<body>
+		<h1>Common Header</h1>
+		<p>Page content here.</p>
+	</body>
+</html>` {
+		t.Logf("\"%s\"", out)
+		t.Fatal("Rendered template is incorrect.")
+	}
+}
+
+func Test_MasterPageWithOptionalSections(t *testing.T) {
+	ve := New()
+
+	Must(ve.Parse("masterpage", `<html>
+	<body>
+		{{optional_template "__header"}}
+		{{template "__body"}}
+		{{optional_template "__footer"}}
+	</body>
+</html>`))
+
+	Must(ve.Parse("homepage", `{{define "__body"}}<p>Page content here.</p>{{end}}
+{{define "__header"}}<h1>Page header</h1>{{end}}
+{{template "masterpage"}}`))
+
+	out, err := ve.render("homepage", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if out != `
+
+<html>
+	<body>
+		<h1>Page header</h1>
+		<p>Page content here.</p>
+
+	</body>
+</html>` {
+		t.Logf("\"%s\"", out)
+		t.Fatal("Rendered template is incorrect.")
+	}
+}
+
+func Test_DataBinding(t *testing.T) {
+	ve := New()
+
+	Must(ve.Parse("masterpage", `<html>
+	<body>
+		{{template "__body" .}}
+		{{.}}
+	</body>
+</html>`))
+
+	Must(ve.Parse("homepage", `{{define "__body"}}<p>Page content here. {{.}}</p>{{end}}
+{{.}}
+{{template "masterpage" .}}
+{{.}}`))
+
+	data := "DATA"
+
+	out, err := ve.render("homepage", data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if out != `
+DATA
+<html>
+	<body>
+		<p>Page content here. DATA</p>
+		DATA
+	</body>
+</html>
+DATA` {
+		t.Logf("\"%s\"", showWhitespace(out))
+		t.Fatal("Rendered template is incorrect.")
+	}
 }
 
 func Test_ParseFiles(t *testing.T) {
